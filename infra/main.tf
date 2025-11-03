f34fcbf70df0b9cf03e11be5fe38a6e213a4e191
Sujay Pillai <sujaypillai@microsoft.com>
2025-11-04 00:03:20 +0800

    Update module name

--- infra/main.tf
+++ infra/main.tf
@@ -7,6 +7,13 @@ terraform {
       version = "4.19.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "sttfstateaspnetmvc"
+    container_name       = "tfstate"
+    key                  = "aspnetmvcapp.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
